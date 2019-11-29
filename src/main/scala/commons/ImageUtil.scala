b9c088dc2599d093297988ae67681e9f299d94d0
Ernesto Chero <informatico240@gmail.com>
2019-11-28 19:14:22 -0500

    Remove unused files

--- src/main/scala/commons/ImageUtil.scala
+++ src/main/scala/commons/ImageUtil.scala
@@ -1,33 +1,6 @@
 package commons
-
-import java.awt.image.BufferedImage
-import java.io.{ File, FileInputStream, InputStream }
-
-import javax.imageio.ImageIO
-
-import scala.util.Try
-
+import java.io.File
 object ImageUtil {
-
-  def loadImage(inputStream: InputStream): BufferedImage = {
-    val bufferedImage = ImageIO.read(inputStream)
-    val width         = bufferedImage.getWidth
-    val height        = bufferedImage.getHeight
-    val img           = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-    for (x <- 0 until width; y <- 0 until height) img.setRGB(x, y, bufferedImage.getRGB(x, y))
-    img
-  }
-
-  def createInputStream(path: String): Option[InputStream] =
-    Try {
-      new FileInputStream(path)
-    }.toOption
-
-  def loadFileImage(path: String): Option[BufferedImage] = {
-    val fileInputStream = createInputStream(path)
-    fileInputStream.map(loadImage)
-  }
-
   def getPathsFromFolder(path: String): List[String] = {
     val file      = new File(path)
     val directory = if (file.isDirectory) Some(file) else None
@@ -36,5 +9,4 @@ object ImageUtil {
       files.map(_.getPath)
     }
   }
-
 }
