97959e44cdadd76557af233c6a1944d3947a1441
ernestochero <ernestochr08@gmail.com>
2019-12-21 16:14:05 -0500

    Updated jar name in script

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 for i in {1..10}
 do
 echo "Running blueSky $i time"
-java -Djava.library.path="/home/ernestochero/opencv-3.4.1/build/lib" -jar target/scala-2.12/BlueSky-assembly-0.1.jar
+java -Djava.library.path="/home/ernestochero/opencv-3.4.1/build/lib" -jar target/scala-2.12/blue-sky-assembly-0.1.jar
 done
